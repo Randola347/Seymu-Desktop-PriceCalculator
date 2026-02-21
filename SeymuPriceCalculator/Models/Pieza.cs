aa1c813b541f7454077445464eb5bb304290ea0b
Randola347 <lpperezo84@gmail.com>
2026-02-21 14:12:02 -0600

    Initial project setup with folder structure and Pieza model

---
+++ SeymuPriceCalculator/Models/Pieza.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeymuPriceCalculator.Models
+{
+    public class Pieza
+    {
+        public double LargoEnVaras { get; set; }
+        public double AnchoEnPulgadas { get; set; }
+        public double GrosorEnPulgadas { get; set; }
+
+        public double TotalPulgadas
+        {
+            get
+            {
+                return LargoEnVaras * 36 * AnchoEnPulgadas * GrosorEnPulgadas;
+            }
+        }
+    }
+}
