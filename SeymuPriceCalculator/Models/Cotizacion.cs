82e4be4cd2f3a7306f1618ee9fef907faa17b5e8
Randola347 <lpperezo84@gmail.com>
2026-02-21 14:15:45 -0600

    Added Cotizacion model with calculation logic and VAT implementation

---
+++ SeymuPriceCalculator/Models/Cotizacion.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SeymuPriceCalculator.Models
+{
+    public class Cotizacion
+    {
+        public string TipoMadera { get; set; }
+        public string NombreCliente { get; set; }
+        public string TelefonoCliente { get; set; }
+
+        public List<Pieza> Piezas { get; set; } = new List<Pieza>();
+
+        public double PrecioPorPulgada { get; set; }
+
+        public double TotalVaras
+        {
+            get
+            {
+                return Piezas.Sum(p => p.LargoEnVaras);
+            }
+        }
+
+        public double TotalPulgadas
+        {
+            get
+            {
+                return Piezas.Sum(p => p.TotalPulgadas);
+            }
+        }
+
+        public double Subtotal
+        {
+            get
+            {
+                return TotalPulgadas * PrecioPorPulgada;
+            }
+        }
+
+        public double IVA
+        {
+            get
+            {
+                return Subtotal * 0.13;
+            }
+        }
+
+        public double TotalFinal
+        {
+            get
+            {
+                return Subtotal + IVA;
+            }
+        }
+
+        public DateTime Fecha { get; set; } = DateTime.Now;
+    }
+}
